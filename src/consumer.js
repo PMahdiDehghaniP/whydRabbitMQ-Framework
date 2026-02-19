2d6d650088a3e1c52f033772c1b36aa3708ffee4
mahdidehghani05421-dev <mahdidehghani05421@gmail.com>
2026-02-19 18:07:48 +0330

    add consumer

---
+++ src/consumer.js
@@ -0,0 +1,28 @@
+const consume = async (channel, config, logger) => {
+  const {
+    queue,
+    handler,
+    prefetch = 1,
+    requeueOnError = false,
+    parser = JSON.parse,
+  } = config;
+
+  await channel.prefetch(prefetch);
+
+  channel.consume(queue, async (msg) => {
+    if (!msg) return;
+
+    try {
+      let content = msg.content.toString();
+      if (parser) content = parser(content);
+
+      await handler(content, msg);
+      channel.ack(msg);
+    } catch (err) {
+      logger.error("Consume error", err);
+      channel.nack(msg, false, requeueOnError);
+    }
+  });
+};
+
+module.exports = { consume };
